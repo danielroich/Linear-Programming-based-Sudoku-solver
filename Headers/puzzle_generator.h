cd10ac98696af081e86a38b3da8c070df27802a9
Daniel Roich <danielroich@gmail.com>
2019-12-21 19:53:55 +0200

    First push

---
+++ Headers/puzzle_generator.h
@@ -0,0 +1,8 @@
+//
+// Created by Daniel on 12/21/2019.
+//
+
+#ifndef EX3_PUZZLE_GENERATOR_H
+#define EX3_PUZZLE_GENERATOR_H
+
+#endif //EX3_PUZZLE_GENERATOR_H
---
+++ Headers/soduko_baord.h
@@ -0,0 +1,8 @@
+//
+// Created by Daniel on 12/21/2019.
+//
+
+#ifndef EX3_SODUKO_BAORD_H
+#define EX3_SODUKO_BAORD_H
+
+#endif //EX3_SODUKO_BAORD_H
