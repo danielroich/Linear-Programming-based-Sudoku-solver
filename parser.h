9ab70d059d7f30441a863da04ca53f4acbe0c1ed
nofarm <nofarm96@gmail.com>
2019-12-26 00:13:46 +0200

    fix include to sudoku

--- parser.h
+++ parser.h
@@ -5,7 +5,7 @@
 #ifndef EX3_PARSER_H
 #define EX3_PARSER_H
 
-#include "soduko_board_actions.h"
+#include "sudoku_board_actions.h"
 #include "backtracking_core_logic.h"
 
 int parse_command(char* command);
