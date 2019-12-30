76fb6eeb3b2b880352088a1848669b1d01b11baf
Daniel Roich <danielroich@gmail.com>
2019-12-30 10:34:16 +0200

    adding seed to main

--- main.c
+++ main.c
@@ -2,14 +2,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-    char* command = malloc((sizeof(char)*1024)); 
+int main(int argc, char *argv) {
+    int seed_num;
+    char* command = malloc((sizeof(char)*1024));
     Board* board = malloc((sizeof(Board)));
+
+    if (argc  <= 1) {
+        return 0;
+    }
+    seed_num = (argv[2] - '0');
+    seed(seed_num);
+
     create_empty_board(board,3,3);
     restart(board);
     print_board(board);
     if(feof(stdin)){exit_game(board);}
-    while(1==1){
+    while(1){
         fgets(command,1024,stdin);
         parse_command(command, board);
         if(feof(stdin)){exit_game(board);}
