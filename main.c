ca6b7f6485a91d2f20a8ad18f99e2ab44ae8ca41
nofarm <nofarm96@gmail.com>
2019-12-31 17:23:12 +0200

    malloc check in main

--- main.c
+++ main.c
@@ -4,9 +4,13 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
+
     int seed_num;
     char* command = malloc((sizeof(char)*1024));
     Board* board = malloc((sizeof(Board)));
+
+    if(command==NULL){printf("Error: malloc has failed\n");exit(0);}
+    if(board==NULL){printf("Error: malloc has failed\n");exit(0);}
 
     if (argc  <= 1) {
         return 0;
@@ -16,11 +20,13 @@ int main(int argc, char **argv) {
 
     create_empty_board(board,3,3);
     restart(board);
+
     if(feof(stdin)){exit_game(board);}
     while(1){
         fgets(command,1024,stdin);
         parse_command(command, board);
         if(feof(stdin)){exit_game(board);}
     }
+
     return 0;
 }
