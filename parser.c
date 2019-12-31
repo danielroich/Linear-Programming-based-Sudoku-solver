bc37b45ff392f60415dd1ecc8c7a5ff307717406
nofarm <nofarm96@gmail.com>
2019-12-31 12:47:15 +0200

    fix unfinished command in parser

--- parser.c
+++ parser.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 /* interpret user input and call actions
 return num op or 0 if invalid command
 if board filled can restart and exit only */
@@ -16,22 +16,49 @@ int parse_command(char* command, Board* board){
     if(token == NULL){free(token);return 0;}
 
     if(strcmp(token, "set") == 0 && !filled){
+
         token = (strtok(NULL, " \t\r\n"));
+        if(token == NULL){
+            printf("Error: invalid command\n");
+            free(token);
+            return 0;}
         x = atoi(token);
+
         token = (strtok(NULL, " \t\r\n"));
+        if(token == NULL){
+            printf("Error: invalid command\n");
+            free(token);
+            return 0;}
         y = atoi(token);
+
         token = (strtok(NULL, " \t\r\n"));
+        if(token == NULL){
+            printf("Error: invalid command\n");
+            free(token);
+            return 0;}
         z = atoi(token);
+
         set_value_user(y,x,z,board);
         free(token);
         return 1;
     }
 
     if(strcmp(token, "hint") == 0 && !filled){
+
         token = (strtok(NULL, " \t\r\n"));
+        if(token == NULL){
+            printf("Error: invalid command\n");
+            free(token);
+            return 0;}
         x = atoi(token);
+
         token = (strtok(NULL, " \t\r\n"));
+        if(token == NULL){
+            printf("Error: invalid command\n");
+            free(token);
+            return 0;}
         y = atoi(token);
+
         hint(y,x,board);
         free(token);
         return 2;
