2b0eea44adebb5f94281c89a1132fa9e794a2cd0
nofarm <nofarm96@gmail.com>
2020-01-01 11:41:15 +0200

    del free(token)

--- parser.c
+++ parser.c
@@ -14,33 +14,29 @@ int parse_command(char* command, Board* board){
 
     token = strtok(command," \t\r\n");
     
-    if(token == NULL){free(token);return 0;}
+    if(token == NULL){return 0;}
 
     if(strcmp(token, "set") == 0 && !filled){
 
         token = (strtok(NULL, " \t\r\n"));
         if(token == NULL){
             printf("Error: invalid command\n");
-            free(token);
             return 0;}
         x = atoi(token);
 
         token = (strtok(NULL, " \t\r\n"));
         if(token == NULL){
             printf("Error: invalid command\n");
-            free(token);
             return 0;}
         y = atoi(token);
 
         token = (strtok(NULL, " \t\r\n"));
         if(token == NULL){
             printf("Error: invalid command\n");
-            free(token);
             return 0;}
         z = atoi(token);
 
         set_value_user(y,x,z,board);
-        free(token);
         return 1;
     }
 
@@ -49,41 +45,34 @@ int parse_command(char* command, Board* board){
         token = (strtok(NULL, " \t\r\n"));
         if(token == NULL){
             printf("Error: invalid command\n");
-            free(token);
             return 0;}
         x = atoi(token);
 
         token = (strtok(NULL, " \t\r\n"));
         if(token == NULL){
             printf("Error: invalid command\n");
-            free(token);
             return 0;}
         y = atoi(token);
 
         hint(y,x,board);
-        free(token);
         return 2;
     }
 
     if(strcmp(token, "validate") == 0 && !filled){
         validate_board(board);
-        free(token);
         return 3;
     }
 
     if(strcmp(token, "restart") == 0){
         restart(board);
-        free(token);
         return 4;
     }
 
     if(strcmp(token, "exit") == 0){
         exit_game(board);
-        free(token);
         return 5;
     }
     
-    free(token);
     /*a command that doesn't match any of the commands defined*/
     printf("Error: invalid command\n");
     return 0;
