c61e0803037b79ac5d012a48727bf6ea95f103c4
Daniel Roich <danielroich@gmail.com>
2020-02-26 16:32:38 +0200

    making the code compile

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
     int seed_num;
     char* command = (char*) malloc((sizeof(char)*1024));
     Board* board = (Board*) malloc((sizeof(Board)));
-    Moves* moves;
+    Moves* moves = (Moves*)malloc(sizeof(Moves));
 
     if(command==NULL){
         printf("Error: malloc has failed\n");
